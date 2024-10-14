5033c5e11a8d8eb7746a12d8184f03e563957d2b
mehditorabiv <mehditorabiv@gmail.com>
2024-10-14 12:10:32 +0300

    setup linter

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,32 +1,32 @@
-import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
 import json from "@rollup/plugin-json";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "rollup-plugin-typescript2";
 
 export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: "dist/index.cjs.js",
-      format: "cjs",
-      sourcemap: true,
-    },
-    {
-      file: "dist/index.esm.js",
-      format: "esm",
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    resolve({
-      browser: true,
-      preferBuiltins: false,
-    }),
-    commonjs(),
-    typescript({
-      useTsconfigDeclarationDir: true,
-    }),
-    json(),
-  ],
-  external: ["fs", "path"],
+	input: "src/index.ts",
+	output: [
+		{
+			file: "dist/index.cjs.js",
+			format: "cjs",
+			sourcemap: true,
+		},
+		{
+			file: "dist/index.esm.js",
+			format: "esm",
+			sourcemap: true,
+		},
+	],
+	plugins: [
+		resolve({
+			browser: true,
+			preferBuiltins: false,
+		}),
+		commonjs(),
+		typescript({
+			useTsconfigDeclarationDir: true,
+		}),
+		json(),
+	],
+	external: ["fs", "path"],
 };
