1814da9523d15b9307b152b558cf4dde87c07dec
mehditorabiv <mehditorabiv@gmail.com>
2024-10-31 22:15:47 +0300

    fix lint

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,41 +6,41 @@ import resolve from "@rollup/plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
 
 export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: "dist/index.cjs",
-      format: "cjs",
-      sourcemap: true,
-      inlineDynamicImports: true,
-      exports: "named",
-    },
-    {
-      file: "dist/index.esm.js",
-      format: "esm",
-      sourcemap: true,
-      inlineDynamicImports: true,
-    },
-  ],
-  plugins: [
-    alias({
-      entries: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
-    }),
-    resolve({
-      browser: false,
-      preferBuiltins: true,
-    }),
-    commonjs(),
-    typescript({
-      useTsconfigDeclarationDir: true,
-      tsconfigOverride: {
-        compilerOptions: {
-          declaration: true,
-          declarationDir: "dist",
-        },
-      },
-    }),
-    json(),
-  ],
-  external: ["fs", "path"],
+	input: "src/index.ts",
+	output: [
+		{
+			file: "dist/index.cjs",
+			format: "cjs",
+			sourcemap: true,
+			inlineDynamicImports: true,
+			exports: "named",
+		},
+		{
+			file: "dist/index.esm.js",
+			format: "esm",
+			sourcemap: true,
+			inlineDynamicImports: true,
+		},
+	],
+	plugins: [
+		alias({
+			entries: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+		}),
+		resolve({
+			browser: false,
+			preferBuiltins: true,
+		}),
+		commonjs(),
+		typescript({
+			useTsconfigDeclarationDir: true,
+			tsconfigOverride: {
+				compilerOptions: {
+					declaration: true,
+					declarationDir: "dist",
+				},
+			},
+		}),
+		json(),
+	],
+	external: ["fs", "path"],
 };
