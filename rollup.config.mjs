2a43e086621f4084f206b308bf695a7c5b58be57
mehditorabiv <mehditorabiv@gmail.com>
2024-10-31 17:56:18 +0300

    fix

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,40 +1,40 @@
+import path from "node:path";
+import alias from "@rollup/plugin-alias";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
-import alias from "@rollup/plugin-alias";
-import path from "node:path";
 
 export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: "dist/index.cjs",
-      format: "cjs",
-      sourcemap: true,
-      inlineDynamicImports: true,
-      exports: "named",
-    },
-    {
-      file: "dist/index.esm.js",
-      format: "esm",
-      sourcemap: true,
-      inlineDynamicImports: true,
-    },
-  ],
-  plugins: [
-    alias({
-      entries: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
-    }),
-    resolve({
-      browser: false,
-      preferBuiltins: true,
-    }),
-    commonjs(),
-    typescript({
-      useTsconfigDeclarationDir: true,
-    }),
-    json(),
-  ],
-  external: ["fs", "path"],
+	input: "src/index.ts",
+	output: [
+		{
+			file: "dist/index.cjs",
+			format: "cjs",
+			sourcemap: true,
+			inlineDynamicImports: true,
+			exports: "named",
+		},
+		{
+			file: "dist/index.esm.js",
+			format: "esm",
+			sourcemap: true,
+			inlineDynamicImports: true,
+		},
+	],
+	plugins: [
+		alias({
+			entries: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+		}),
+		resolve({
+			browser: false,
+			preferBuiltins: true,
+		}),
+		commonjs(),
+		typescript({
+			useTsconfigDeclarationDir: true,
+		}),
+		json(),
+	],
+	external: ["fs", "path"],
 };
