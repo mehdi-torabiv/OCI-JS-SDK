9e41e4829e3fcd081d0af2366587df2ceabcb4c6
mehditorabiv <mehditorabiv@gmail.com>
2024-10-31 16:57:02 +0300

    get profiles by wallet account

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,6 @@
-import dotenv from "dotenv";
-import { OciClient } from "./services/oci";
-dotenv.config();
-
-const ociClient = new OciClient({
-	chainId: 11155111,
-	appPrivateKey: process.env.DEFAULT_APP_DEVELOPER_PRIVATE_KEY as `0x${string}`,
-});
-
-async function fetchUserProfiles() {
-	try {
-		const profiles = await ociClient.getUserProfiles(
-			"discord",
-			"973993299281076285",
-		);
-		console.log({ profiles });
-	} catch (error) {
-		console.error("Error fetching user profiles:", error);
-	}
-}
-
-fetchUserProfiles();
+export * from "./services/eas";
+export * from "./services/ethers";
+export * from "./services/lit";
+export * from "./services/oid";
+export * from "./services/oci";
+export * from "./services/fetch";
